6303f7ac53011be8d19566f03f189b5f424edcb9
tozy10 <ernesttozy@gmail.com>
2024-04-21 21:24:20 +0200

    Added poll urls on dashboard

--- app/dashboards/poll_url_dashboard.rb
+++ app/dashboards/poll_url_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class UserDashboard < Administrate::BaseDashboard
+class PollUrlDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -9,12 +9,8 @@ class UserDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    balance: Field::Number,
-    email: Field::String,
-    first_name: Field::String,
-    ecocash_number: Field::String,
-    poll_urls: Field::HasMany,
-    last_name: Field::String,
+    poll_url: Field::String,
+    user: Field::BelongsTo,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
@@ -25,36 +21,28 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    first_name
-    last_name
-    email
-    ecocash_number
-    balance
+    id
+    poll_url
+    user
+    created_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    balance
-    email
-    first_name
-    ecocash_number
-    last_name
+    poll_url
+    user
     created_at
     updated_at
-    poll_urls
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    first_name
-    last_name
-    ecocash_number
-    email
-    balance
+    poll_url
+    user
   ].freeze
 
   # COLLECTION_FILTERS
@@ -69,10 +57,10 @@ class UserDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how users are displayed
+  # Overwrite this method to customize how poll urls are displayed
   # across all pages of the admin dashboard.
   #
-  def display_resource(user)
-    user.first_name + " " + user.last_name
-  end
+  # def display_resource(poll_url)
+  #   "PollUrl ##{poll_url.id}"
+  # end
 end
